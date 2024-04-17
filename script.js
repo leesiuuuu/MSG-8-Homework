3219080bc9ee005d0b30d73366687b91435e6c48
leesiuuuu <s24028@gsm.hs.kr>
2024-04-17 16:13:32 +0900

    test123

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
+const imgElement = document.getElementById("img");
+var input = document.getElementById("user-id");
+var input1 = document.getElementById("password");
+var newid = document.getElementById("new-user-id");
+var newpwd = document.getElementById("new-password");
 
 loginButton.addEventListener("click", (event) => {
     event.preventDefault();
@@ -7,9 +12,20 @@ loginButton.addEventListener("click", (event) => {
     const username = document.getElementById("user-id").value;
     const password = document.getElementById("password").value;
 
-    if (username === "llssww0119" && password === "ll3537878*") {
-        document.getElementById("img").src = "Loginsucceed.png";
+    if (username === "user-id" && password === "password") {
+        imgElement.src = "Loginsucceed.png";
+        input.value = null;
+        input1.value = null;
+        setTimeout(function() {
+            imgElement.src = "Test_image.png";
+        }, 3000);
     } else {
-        document.getElementById("img").src = "whoareyou.png";
+        imgElement.src = "whoareyou.png";
+        input1.value = null;
+        input.value = null;
+        setTimeout(function() {
+            imgElement.src = "Test_image.png";
+        }, 3000);
     }
 });
+
